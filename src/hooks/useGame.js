41f2a43087463bfdf1eb83bc0b176e790516b019
BohdanBychak <ipz232_bbm@student.ztu.edu.ua>
2025-10-30 13:14:00 +0200

    Refactor GamePage to use custom hooks for game logic; add useGame and usePlayerActions hooks

---
+++ src/hooks/useGame.js
@@ -0,0 +1,32 @@
+import {useState} from "react";
+import playerHand from "../components/PlayerHand/PlayerHand.jsx";
+import {shuffle} from "../utils/GameUtil.js";
+import allCards from "../data/cards.js";
+
+export default function useGame(initialState) {
+    const [deck, setDeck] = useState([...initialState]);
+    const [result, setResult] = useState(null);
+    const [reveal, setReveal] = useState(false);
+
+    const dealInitialCards = (playerHand, dealerHand) => {
+        let currentDeck = [...deck];
+
+        const playerCards = [currentDeck.shift(), currentDeck.shift()];
+        const dealerCards = [currentDeck.shift(), currentDeck.shift()];
+
+        setDeck(currentDeck);
+        playerHand.setHand(playerCards);
+        dealerHand.setHand(dealerCards);
+    };
+
+    const onReload = (playerHand, dealerHand) => {
+        const reshuffled = shuffle([...allCards]);
+        setDeck(reshuffled);
+        playerHand.clearHand();
+        dealerHand.clearHand();
+        setResult(null);
+        setReveal(false);
+    }
+
+    return {deck, setDeck, result, setResult, reveal, setReveal, dealInitialCards, onReload};
+}
--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,92 +1,24 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect} from "react";
 import DealerHand from "../components/DealerHand/DealerHand.jsx";
 import PlayerHand from "../components/PlayerHand/PlayerHand.jsx";
 import GameControls from "../components/GameControls/GameControls.jsx";
 import allCards from "../data/cards.js";
-import {calculatePoints, shuffle} from "../utils/GameUtil.js";
 import Modal from "../components/Modal/Modal.jsx";
 import ResultPage from "./ResultPage.jsx";
-import useDeck from "../hooks/useDeck.js";
 import useHand from "../hooks/useHand.js";
+import usePlayerActions from "../hooks/usePlayerActions.js";
+import useGame from "../hooks/useGame.js";
 
 
 function GamePage() {
-    const {deck, shuffleDeck, getCardFromDeck, setDeck} = useDeck(shuffle([...allCards]));
     const playerHand = useHand();
     const dealerHand = useHand();
-    const [reveal, setReveal] = React.useState(false);
+    const {deck, setDeck, result, setResult, reveal, setReveal, dealInitialCards, onReload} = useGame(allCards);
+    const {onHit, onStand} = usePlayerActions(deck, setDeck, playerHand, dealerHand, setReveal, setResult);
 
-    const [result, setResult] = useState(null);
-
-
-    const dealInitialCards = () => {
-        let currentDeck = [...deck];
-
-        const playerCard1 = currentDeck.shift();
-        const playerCard2 = currentDeck.shift();
-        const dealerCard1 = currentDeck.shift();
-        const dealerCard2 = currentDeck.shift();
-
-        setDeck(currentDeck);
-        playerHand.setHand([playerCard1, playerCard2]);
-        dealerHand.setHand([dealerCard1, dealerCard2]);
-    };
-
-
-    const onReload = () => {
-        const reshuffled = shuffle([...allCards]);
-        setDeck(reshuffled);
-        playerHand.clearHand();
-        dealerHand.clearHand();
-        dealInitialCards();
-        setResult(null);
-        setReveal(false);
-    };
-
-    const onHit = () => {
-        const card = getCardFromDeck(deck);
-
-        const newPlayerCards = [...playerHand.hand, card];
-        playerHand.setHand(newPlayerCards);
-
-        if (calculatePoints(newPlayerCards) > 21) {
-            setReveal(true);
-            setResult("Bust! Dealer wins.");
-        }
-    };
-
-    const onStand = () => {
-        let currentDeck = [...deck];
-        let currentDealerCards = [...dealerHand.hand];
-        let dealerPoints = dealerHand.points;
-        setReveal(true);
-
-        while (dealerPoints < 17) {
-            const card = currentDeck.shift();
-            currentDealerCards.push(card);
-            dealerPoints = calculatePoints(currentDealerCards);
-        }
-
-        dealerHand.setHand(currentDealerCards);
-        setDeck(currentDeck);
-
-        const playerPoints = playerHand.points;
-
-        if (dealerPoints > 21 || playerPoints > dealerPoints) {
-            setResult("You win!");
-        } else if (playerPoints < dealerPoints) {
-            setResult("Dealer wins!");
-        } else{
-            setResult("It's a tie!");
-        }
-    }
 
     useEffect(() => {
-        shuffleDeck();
-    }, []);
-
-    useEffect(() => {
-        dealInitialCards();
+        dealInitialCards(playerHand, dealerHand);
     }, []);
 
     return (
@@ -94,8 +26,8 @@ function GamePage() {
             <DealerHand cards={dealerHand.hand} reveal={reveal}/>
             <PlayerHand name="Player" cards={playerHand.hand}/>
             <GameControls onHit={onHit} onStand={onStand}/>
-            <Modal isOpen={result} title="Result" onClose={() => {setResult(false); onReload();}}>
-                <ResultPage result={result} onRestart={onReload}/>
+            <Modal isOpen={result} title="Result" onClose={() => {setResult(null); onReload(playerHand, dealerHand);}}>
+                <ResultPage result={result} onRestart={() => {onReload(playerHand, dealerHand)}}/>
             </Modal>
         </div>
     );
