b95b3dcfaa2f2dc41936a2a4191c9d773fefe0db
BohdanBychak <ipz232_bbm@student.ztu.edu.ua>
2025-10-30 09:50:32 +0200

    Add deployment scripts and homepage URL to package.json; configure base path in vite.config.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import './index.css';
-import {BrowserRouter, Route, Routes, Navigate} from 'react-router';
+import {BrowserRouter, Route, Routes, Navigate, HashRouter} from 'react-router';
 import StartPage from './pages/StartPage/StartPage.jsx';
 import Layout from './pages/Layout/Layout.jsx';
 import GamePage from './pages/GamePage.jsx';
@@ -9,14 +9,15 @@ import ResultPage from './pages/ResultPage.jsx';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route element={<Layout/>}>
+                    <Route index path="/" element={<StartPage/>}/>
                     <Route path="/start" element={<StartPage/>}/>
                     <Route path="/game" element={<GamePage/>}/>
                     <Route path="/result" element={<ResultPage/>}/>
                 </Route>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     </StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+
+const repoName = 'blackjack';
+const isProduction = process.env.NODE_ENV === 'production';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    base: isProduction ? `/${repoName}/` : '/',
 })
